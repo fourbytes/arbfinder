eeb2b5c569dca459c5240936fa4ec523d78b0be6
Oscar Rainford <oscar@fourbytes.me>
2018-06-12 23:28:43 +1000

    Some nice improvements all round, moved more into classes, better handling of base currencies other than btc.

--- arbfinder.py
+++ arbfinder.py
@@ -7,7 +7,7 @@ from icecream import ic
 from utils import (
     format_btc, Market, ArbitrageOpportunity,
     GREEN, RED, END, BLUE, BOLD, PURPLE,
-    cryptopia, bittrex, binance, Pair
+    cryptopia, bittrex, binance, Pair, PairMarkets
 )
 
 
@@ -15,11 +15,17 @@ log = logging.getLogger(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
-MIN_VOLUME = 5
 BASE_CURRENCIES = ['BTC', 'ETH']
 TRANSFER_CURRENCIES = ['DOGE', 'LTC', 'ETH', 'DASH', 'BITB', 'XMR', 'ZEC', 'XRP', 'NEO']
 
-markets = []
+pair_markets = {}
+
+def get_or_create_pm(pair):
+    for p, pm in pair_markets.items():
+        if p == pair:
+            return pm
+    pair_markets[pair] = PairMarkets(pair=pair)
+    return pair_markets[pair]
 
 try:
     tradeogre_data = requests.get('https://tradeogre.com/api/v1/markets').json()
@@ -29,9 +35,10 @@ else:
     for market in tradeogre_data:
         pair, market_data = list(market.items())[0]
         base, asset = pair.split('-')
+        pair = Pair(base, asset)
 
-        markets.append(Market(
-            Pair(base, asset),
+        get_or_create_pm(pair).markets.append(Market(
+            pair,
             bid=float(market_data['bid']),
             ask=float(market_data['ask']),
             last=float(market_data['price']),
@@ -43,8 +50,10 @@ cryptopia_markets = cryptopia.request('GetMarkets').json()['Data']
 for market in cryptopia_markets:
     elements = market['Label'].split('/')
     asset, base = '/'.join(elements[:-1]), elements[-1]
-    markets.append(Market(
-        Pair(base, asset),
+    pair = Pair(base, asset)
+
+    get_or_create_pm(pair).markets.append(Market(
+        pair,
         market['BidPrice'],
         market['AskPrice'],
         market['LastPrice'],
@@ -55,8 +64,10 @@ for market in cryptopia_markets:
 bittrex_markets = bittrex.request('public/getmarketsummaries').json()['result']
 for market in bittrex_markets:
     base, asset = market['MarketName'].split('-')
-    markets.append(Market(
-        Pair(base, asset),
+    pair = Pair(base, asset)
+
+    get_or_create_pm(pair).markets.append(Market(
+        pair,
         market['Bid'],
         market['Ask'],
         market['Last'],
@@ -81,8 +92,10 @@ for market in binance_markets:
     ask_qty = float(market['askQty'])
     base_qty = (((bid_qty + ask_qty) / 2) * avg_price) * 100
 
-    markets.append(Market(
-        Pair(base, asset),
+    pair = Pair(base, asset)
+
+    get_or_create_pm(pair).markets.append(Market(
+        pair,
         bid_price,
         ask_price,
         avg_price,
@@ -90,49 +103,39 @@ for market in binance_markets:
         'binance'
     ))
 
-markets_pairs = [
-    list(v) for l,v in groupby(
-        sorted(markets, key=lambda x: str(x.pair)),
-        lambda x: str(x.pair))]
 
-arb_opportunities = []
-for pair in markets_pairs:
-    if len(pair) < 2:
-        log.debug(f'length of markets too low: {pair}')
-        continue
-    if pair[0].pair.base not in BASE_CURRENCIES:
-        log.debug(f'base not in BASE_CURRENCIES: {pair[0].pair.base}')
-        continue
-
-    price_sorted = sorted(pair, key=lambda x: x.last)
-    market1 = price_sorted[0]
-    market2 = price_sorted[-1]
-
-    if market1.volume < MIN_VOLUME or market2.volume < MIN_VOLUME:
-        log.debug(f'volume too low: {market1.volume} | {market2.volume}')
-        continue
-
-    diff = abs(market1.last - market2.last)
-    diff_pct = diff / max(market1.last, market2.last)
-
-    arb_opportunities.append(ArbitrageOpportunity(market1, market2, diff_pct))
+#arb_opportunities = []
+#for pair in markets_pairs:
+#    if len(pair) < 2:
+#        log.debug(f'length of markets too low: {pair}')
+#        continue
+#    if pair[0].pair.base not in BASE_CURRENCIES:
+#        log.debug(f'base not in BASE_CURRENCIES: {pair[0].pair.base}')
+#        continue
+#
+#    price_sorted = sorted(pair, key=lambda x: x.last)
+#    market1 = price_sorted[0]
+#    market2 = price_sorted[-1]
+#
+#    if market1.volume < MIN_VOLUME or market2.volume < MIN_VOLUME:
+#        log.debug(f'volume too low: {market1.volume} | {market2.volume}')
+#        continue
+#
+#    diff = abs(market1.last - market2.last)
+#    diff_pct = diff / max(market1.last, market2.last)
+#
+#    arb_opportunities.append(ArbitrageOpportunity(market1, market2, diff_pct))
 
 if __name__ == "__main__":
-    f_arb_opportunities = arb_opportunities
-    SRC = None # "bittrex"  #"bittrex"
-    if SRC:
-        f_arb_opportunities = list(
-            filter(lambda a: a.market1.exchange == SRC,
-                   arb_opportunities))
-
-    for arb in sorted(f_arb_opportunities, key=lambda a: a.diff_pct, reverse=True)[:7]:
-        print(f"{BOLD}{arb.pair} +{round(arb.diff_pct * 100, 2)}%{END}")
-        m1color = RED if arb.market1.last > arb.market2.last else GREEN
-        m2color = GREEN if arb.market1.last >= arb.market2.last else RED
-        print(f" {m1color}+ {arb.market1.exchange}: last: {format_btc(arb.market1.last)} (vol {round(arb.market1.volume, 2)}BTC){END}")
-        print(f" {m2color}- {arb.market2.exchange}: last: {format_btc(arb.market2.last)} (vol {round(arb.market2.volume, 2)}BTC){END}")
-
-    lowest_b = sorted(arb_opportunities, key=lambda a: a.diff_pct)
+    arbs = []
+    for pm in pair_markets.values():
+        arb = pm.get_best_arbitrage()
+        if arb:
+            arbs.append(arb)
+    
+    for arb in sorted(arbs, key=lambda a: a.diff_pct, reverse=True)[:7]:
+        arb.print()
+    lowest_b = sorted(arbs, key=lambda a: a.diff_pct)
     lowest = filter(lambda y: y.pair.asset in TRANSFER_CURRENCIES and y.pair.base == 'BTC', lowest_b)
 
     print()
@@ -140,5 +143,5 @@ if __name__ == "__main__":
     print()
     for arb in list(lowest)[:3]:
         print(f"{BOLD}{arb.pair.asset} {round(arb.diff_pct * 100, 2)}%{END}")
-        print(f"{BLUE} * {arb.market1.exchange}: {format_btc(arb.market1.last)} {END}")
-        print(f"{BLUE} * {arb.market2.exchange}: {format_btc(arb.market2.last)} {END}")
+        print(f"{BLUE} * {arb.buy_market.exchange}: {format_btc(arb.buy_market.last)} {END}")
+        print(f"{BLUE} * {arb.sell_market.exchange}: {format_btc(arb.sell_market.last)} {END}")
--- utils.py
+++ utils.py
@@ -16,10 +16,14 @@ PURPLE = '\033[35m'
 END = '\033[0m'
 
 
+class NotEnoughMarketsException(Exception):
+    pass
+
+
 class Pair(object):
     def __init__(self, base, asset):
-        self.base = base
-        self.asset = asset
+        self.base = base.upper()
+        self.asset = asset.upper()
 
     def ab(self):
         return sorted([self.base, self.asset])
@@ -39,6 +43,9 @@ class Pair(object):
     def __str__(self):
         return f'{self.base}/{self.asset}'
 
+    def __hash__(self):
+        return hash(repr(self))
+
 
 class Exchange(object):
     def __init__(self, name):
@@ -53,13 +60,15 @@ class Exchange(object):
 
 class Market(object):
     def __init__(self, pair: Pair, bid: float,
-                 ask: float, last: float, volume: float, exchange: str):
+                 ask: float, last: float, volume: float, exchange: str,
+                 wallet_active: bool = True):
         self.pair = pair
         self.bid = bid
         self.ask = ask
         self.last = last
         self.volume = volume
         self.exchange = exchange
+        self.wallet_active = wallet_active
 
     def get_ticker(self):
         return f"{self.exchange}:{self.get_pair()}"
@@ -68,15 +77,62 @@ class Market(object):
         return f'Market<{repr(self.pair)} Exchange<{self.exchange}>>'
 
 
+class PairMarkets(object):
+    def __init__(self, pair: Pair):
+        self.pair = pair
+        self.markets = []
+
+    def get_acceptable_markets(self, min_volume: float = 5.0):
+        return filter(lambda m: m.volume >= min_volume, self.markets)
+
+    def get_best_arbitrage(self):
+        markets_by_price = sorted(self.get_acceptable_markets(),
+                                  key=lambda x: x.last)
+        if len(markets_by_price) < 2:
+            return None
+
+        buy_market = markets_by_price[0]
+        sell_market = markets_by_price[-1]
+        mid_markets = markets_by_price[1:-1] if len(markets_by_price) >= 3 else []
+        
+        diff = abs(buy_market.ask - sell_market.bid)
+        diff_pct = diff / abs((buy_market.ask + sell_market.bid) / 2)
+        
+        # Returns: tuple(Market<buy>, Market<sell>)
+        return ArbitrageOpportunity(buy_market, sell_market, mid_markets, diff_pct)
+
+
 class ArbitrageOpportunity(object):
-    def __init__(self, market1, market2, diff_pct):
-        self.market1 = market1
-        self.market2 = market2
+    def __init__(self, buy_market: Market, sell_market: Market,
+                 mid_markets: [Market], diff_pct: float):
+        self.buy_market = buy_market
+        self.sell_market = sell_market
+        self.mid_markets = mid_markets
         self.diff_pct = diff_pct
     
     @property
     def pair(self):
-        return self.market1.pair
+        return self.buy_market.pair
+
+    def print(self):
+        if self.pair.base == 'BTC':
+            format_base = format_btc
+        elif self.pair.base in ('USDT', 'USD'):
+            format_base = lambda v: f"{self.pair.base}${round(v, 2)}"
+        else:
+            format_base = lambda v: f"{v}{self.pair.base}"
+
+        # Header
+        print(f"{BOLD}{self.pair} +{round(self.diff_pct * 100, 2)}%{END}")
+
+        # Buy Market
+        print(f" {GREEN}+ {self.buy_market.exchange}: ask: {format_base(self.buy_market.ask)} (vol {format_base(round(self.buy_market.volume, 2))}){END}")
+        
+        for m in self.mid_markets:
+            print(f" * {m.exchange}: ask: {format_base(m.ask)} (vol {format_base(round(m.volume, 2))}){END}")
+
+        # Sell Market
+        print(f" {RED}- {self.sell_market.exchange}: bid: {format_base(self.sell_market.bid)} (vol {format_base(round(self.sell_market.volume, 2))}){END}")
 
 
 def format_btc(price):
